b835e2f630e1e237d1e83be95103cad491c6deb0
G00405094 <G00405094@atu.ie>
2023-11-22 15:15:42 +0000

    Set up docker

---
+++ src/main/java/ie/atu/week9_containers/Week9ContainersApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.week9_containers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week9ContainersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week9ContainersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/week9_containers/Week9ContainersApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.week9_containers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week9ContainersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
